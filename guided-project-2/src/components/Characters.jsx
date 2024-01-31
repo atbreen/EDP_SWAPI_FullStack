a21d33031644ca77da13896b1aefdb306c6220f6
April B <aprilt.breen@gmail.com>
2024-01-31 13:19:56 -0500

    commit updates

--- guided-project-2/src/components/Characters.jsx
+++ guided-project-2/src/components/Characters.jsx
@@ -1,3 +1,49 @@
-export default function Characters() {
-    return 1
+// imports
+
+export default function Characters(characters) {
+    // functions
+function getCharacters() {
+    let characters = [{
+        "id": 1,
+        "name": "Luke Skywalker",
+        "gender": "male",
+        "skin_color": "fair",
+        "hair_color": "blond",
+        "height": "172",
+        "eye_color": "blue",
+        "mass": "77",
+        "homeworld": 1,
+        "birth_year": "19BBY"
+    },
+    {
+        "id": 2,
+        "name": "C-3PO",
+        "gender": "n/a",
+        "skin_color": "gold",
+        "hair_color": "n/a",
+        "height": "167",
+        "eye_color": "yellow",
+        "mass": "75",
+        "homeworld": 1,
+        "birth_year": "112BBY"
+    },
+    {
+        "id": 3,
+        "name": "R2-D2",
+        "gender": "n/a",
+        "skin_color": "white, blue",
+        "hair_color": "n/a",
+        "height": "96",
+        "eye_color": "red",
+        "mass": "32",
+        "homeworld": 8,
+        "birth_year": "33BBY"
+    },]
+    
+    return characters
+}
+    
+    return (
+        1
+    )
 }
