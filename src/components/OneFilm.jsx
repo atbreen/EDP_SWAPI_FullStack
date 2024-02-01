961ac4cd5e9fa4de3ba19ff44af8998368e94517
Zachary Mowry <113538204+zmowry@users.noreply.github.com>
2024-02-01 11:11:27 -0500

    filmin

--- src/components/OneFilm.jsx
+++ src/components/OneFilm.jsx
@@ -1,17 +1,32 @@
 import { React, useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import useGetDocument from '../utils/useGetDocument'
+import useGetCrossDocument from '../utils/useGetCrossDocument'
 
 export default function OneFilm() {
     const {id} = useParams()
     const film = useGetDocument("films", id)
+    const planetList = useGetCrossDocument("films", id, "planets")
+    const charactersList = useGetCrossDocument("films", id, "characters")
    
     return (
         //  if film exists
         film !== null ? 
         <>
             <section id="charactersList">
-                Film info goes here!
+                <p>
+                    <h1>
+                        {film.title}
+                    </h1>
+                </p>
+                <p>
+                    <h2>Characters</h2>
+                </p>
+                <p>
+                    <h3>{charactersList.map(character => 
+                        {character.name}
+                    )}</h3>
+                </p>
             </section>
         </> : null
     )
